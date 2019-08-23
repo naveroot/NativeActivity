55d52ea8d4634adf712d7857eb33e2cf252517b4
naveroot <naveroot@gmail.com>
2019-08-23 10:47:56 +0300

    Add Activity and City models

---
+++ db/migrate/20190823074659_create_activities.rb
@@ -0,0 +1,12 @@
+class CreateActivities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :activities do |t|
+      t.references :city, null: false, foreign_key: true
+      t.string :title
+      t.string :photo
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
